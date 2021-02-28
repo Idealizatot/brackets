e8e436f2d032cdebfed41d9c4676498c1026d840
Idealizatot <ideal103.5@gmail.com>
2021-02-28 17:44:44 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsTypes = bracketsConfig.reduce((bracketsTypes, bracketsArray) => {
+    bracketsTypes[bracketsArray[0]] = {close : bracketsArray[1]};
+    if (bracketsArray[0] === bracketsArray[1]) {
+      bracketsTypes[bracketsArray[0]].double = true;
+    }
+    
+    return bracketsTypes;
+  }, []);
+
+  let bracketsStack = [];
+  let flag = true;
+  str.split('').forEach(element => {
+    if(element in bracketsTypes && !bracketsTypes[element].double) {
+      bracketsStack.push(element)
+    } else if (
+        bracketsTypes[element]?.double 
+        && bracketsStack.length 
+        && bracketsStack[bracketsStack.length - 1] === element) 
+      {
+      bracketsStack.pop()
+      
+    } else if (bracketsTypes[element]?.double) {
+      bracketsStack.push(element)
+    } else {
+      let lastElement = bracketsStack.pop()
+      if (bracketsTypes[lastElement]?.close !== element) {
+        if (bracketsTypes[lastElement]?.double) {
+          bracketsStack.push(element)
+        } else {
+          flag = false
+        }
+      }
+    }
+  });
+
+  if (bracketsStack.length) {
+    flag = false
+  }
+
+  return flag
 }
